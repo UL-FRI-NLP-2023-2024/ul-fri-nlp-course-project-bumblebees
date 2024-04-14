b6e7193077d7fc7d824ba4f09314823bceeac013
KarmenFrank <karmen.frank@gmail.com>
2024-04-14 17:36:41 +0200

    Dodana koda za preprost klasifikator (torch Linear) za osnovni model - treniranje in validacija.

--- code/base_model_train.py
+++ code/base_model_train.py
@@ -1,16 +1,92 @@
 from sentence_transformers import SentenceTransformer
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+from sklearn.metrics import f1_score
 
-from utils import prepare_dataset
+from utils import prepare_dataset, ClassifyingDataset
 
+
+# Set parameters:
+input_dim = 512
+output_dim = 3
+lr = 0.001
+epochs = 3
+batch_size = 8
 
 # Determine device:
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device: ", device)
+
+# TODO: preveriti moramo, ali je to primerno - samo en Linear
+class Classifier(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.linear = nn.Linear(input_dim, output_dim)
+    
+    def forward(self, x):
+        x = self.linear(x)
+        return x
+
+
+def train(train_dataloader, val_dataloader, model, criterion, optimizer):
+    for epoch in range(epochs):
+        # Training:
+        model.train()
+        all_loss = 0.0
+        for embds, labels in train_dataloader:
+            # morde from_numpy
+            embds, labels = embds.to(device), labels.to(device).long()
+            optimizer.zero_grad()
+            outputs = model(embds)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            all_loss += loss.item()
+        print(f"Epoch {epoch+1}, training loss {all_loss/len(train_dataloader)}.")
+
+        # Validation:
+        model.eval()
+        val_predictions = []
+        val_true = []
+        best_score = 0.0
+        with torch.no_grad():
+            for embds, labels in val_dataloader:
+                embds, labels = embds.to(device), labels.to(device).long()
+                outputs = model(embds)
+                probs = torch.softmax(outputs, dim=1)
+                _, predictions = torch.max(probs, dim=1)
+                val_predictions.extend(predictions.cpu().numpy())
+                val_true.extend(labels.cpu().numpy())
+        val_f1 = f1_score(val_true, val_predictions, average='weighted')
+        print(f"Validation F1 score: {val_f1}")
+
+        if best_score < val_f1:
+            print(f"--Found a better model at epoch {epoch+1}!")
+            torch.save(model.state_dict(), 'classifiers/classifier_base_model.pth')
 
 
 if __name__=='__main__':
     # Loading training and validation dataset:
     train_text, train_labels, val_text, val_labels = prepare_dataset(train=True)
 
-    # Loading model:
-    model = SentenceTransformer("sentence-transformers/distiluse-base-multilingual-cased-v2")
+    # Loading base model:
+    base_model = SentenceTransformer("sentence-transformers/distiluse-base-multilingual-cased-v2")
+    train_embds = base_model.encode(train_text)
+    val_embds = base_model.encode(val_text)
+
+    # Preparing DataLoaders:
+    train_dataset = ClassifyingDataset(train_embds, np.asarray(train_labels, dtype=float))
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    val_dataset = ClassifyingDataset(val_embds, np.asarray(val_labels, dtype=float))
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    # Initializing classifying model, loss and optimizer:
+    model = Classifier(input_dim, output_dim).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    # Training loop:
+    train(train_dataloader, val_dataloader, model, criterion, optimizer)
--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,19 @@
 import numpy as np
 from datasets import load_dataset
+from torch.utils.data import Dataset
+
+
+# Custom dataset:
+class ClassifyingDataset(Dataset):
+    def __init__(self, embds, labels) -> None:
+        self.embds = embds
+        self.labels = labels
+    
+    def __len__(self):
+        return len(self.embds)
+    
+    def __getitem__(self, index):
+        return self.embds[index], self.labels[index]
 
 
 #TODO: find a faster way
@@ -20,16 +34,20 @@ def prepare_dataset(train):
     n_train = round(0.7 * dataset_len)
     n_val = round(0.1 * dataset_len)
 
+    n_train = 1000
+    n_val = 100
+    end = 1400
+
     # TODO: set to full length (for example replace 5000)
     # Train and validation set:
     if train:
-        train_text, train_labels = dataset['train']['content'][0:5000], dataset['train']['sentiment'][0:5000]
-        val_text, val_labels = dataset['train']['content'][5000:5500], dataset['train']['sentiment'][5000:5500]
+        train_text, train_labels = dataset['train']['content'][0:n_train], dataset['train']['sentiment'][0:n_train]
+        val_text, val_labels = dataset['train']['content'][n_train:n_train+n_val], dataset['train']['sentiment'][n_train:n_train+n_val]
         train_labels = replace_labels(train_labels)
         val_labels = replace_labels(val_labels)
         return train_text, train_labels, val_text, val_labels
     
     # Test set:
-    test_text, test_labels = dataset['train']['content'][5500:6000], dataset['train']['sentiment'][5500:6000]
+    test_text, test_labels = dataset['train']['content'][n_train+n_val:end], dataset['train']['sentiment'][n_train+n_val:end]
     test_labels = replace_labels(test_labels)
     return test_text, test_labels
