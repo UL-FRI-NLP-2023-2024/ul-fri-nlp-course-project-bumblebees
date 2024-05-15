e62f4dc6bf9a025afeb38ad93740bf1615c7857d
KarmenFrank <karmen.frank@gmail.com>
2024-05-15 19:51:16 +0200

    Dodana dva grafa za GPL.

--- code/visualize_GPL_results.py
+++ code/visualize_GPL_results.py
@@ -3,10 +3,7 @@ from matplotlib import pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 
 
-def draw_graph(results, base_model_f1):
-    base_model = "SloBERTa"
-    T5 = "Boshko"
-
+def draw_graph(results, base_model_f1, base_model, T5):
     fig = plt.figure(figsize=(10,5))
     plt.title(f"GPL_{T5}_{base_model}")
     plt.plot(results[0,:], np.ones(len(results[0,:])) * base_model_f1, label=base_model, color='r')
@@ -17,22 +14,32 @@ def draw_graph(results, base_model_f1):
     plt.xticks(results[0,:], x_names)
 
     plt.ylabel("F1-score")
-    # ys = np.linspace(min(results[1,:]), max(results[1,:]), 8)
-    # y_names = [f"{y:.4f}" for y in ys]
-    # plt.yticks(ys, y_names)
     for i in range(14):
         plt.annotate(f"{results[1,i]:.4f}", results[:,i])
 
     plt.annotate(f"{base_model_f1:.4f}", (results[0,0],base_model_f1+0.001), color='r')
 
     plt.legend(loc='lower right')
+    # plt.legend(loc='center right')
     plt.savefig(f"report/fig/GPL_{T5}_{base_model}", dpi=300)
     plt.show()
 
 
 if __name__=="__main__":
-    path = "data/results_gpl_boshko_sloberta.txt"
-    base_model_f1 = 0.5867172613808097
+    # path = "data/results_gpl_boshko_sloberta.txt"
+    # base_model_f1 = 0.5867172613808097
+    # base_model = "SloBERTa"
+    # T5 = "Boshko"
+
+    path = "data/results_gpl.txt"
+    base_model_f1 = 0.5637068742661127
+    base_model = "paraphrase"
+    T5 = "14langs"
+
+    # path = "data/results_gpl_boshko.txt"
+    # base_model_f1 = 0.5637068742661127
+    # base_model = "paraphrase"
+    # T5 = "Boshko"
 
     results = np.zeros((2, 14))
 
@@ -48,4 +55,4 @@ if __name__=="__main__":
             results[1, i] = float(f1)
             i+=1
 
-    draw_graph(results, base_model_f1)
+    draw_graph(results, base_model_f1, base_model, T5)
